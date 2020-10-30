5952ea1d421f72652bd62ac9727edbf1d183a81d
Ryan Fuhrman <rsfuhrman10@gmail.com>
2020-10-30 12:32:45 -0400

    Added colors and query selectors'

---
+++ main.js
@@ -0,0 +1,23 @@
+const colorSpan = document.querySelector(".color-span");
+const colorBtn = document.querySelector(".color-btn");
+
+const colors = [
+  { name: "red", hex: "#FF0000" },
+  { name: "orange", hex: "#FFA500" },
+  { name: "yellow", hex: "#FFFF00" },
+  { name: "green", hex: "#008000" },
+  { name: "blue", hex: "#0000FF" },
+  { name: "purple", hex: "#800080" },
+  { name: "cyan", hex: "#00FFFF" },
+  { name: "darkblue", hex: "#0000A0" },
+  { name: "lightblue", hex: "#ADD8E6" },
+  { name: "lime", hex: "#00FF00" },
+  { name: "magenta", hex: "#FF00FF" },
+  { name: "silver", hex: "#C0C0C0" },
+  { name: "grey", hex: "#808080" },
+  { name: "brown", hex: "#A52A2A" },
+  { name: "maroon", hex: "#800000" },
+  { name: "olive", hex: "#808000" },
+];
+
+colorBtn.addEventListener("click", () => {});
